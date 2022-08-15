e270a48f483b2837e057ee4ba3147e7cff2c8e62
Baghina Radu Adrian <softmixt@gmail.com>
2022-08-15 18:12:24 +0200

    Update ProductsData.php

--- App/DTO/ProductsData.php
+++ App/DTO/ProductsData.php
@@ -62,17 +62,4 @@ class ProductsData
 		return $newProductsFormatted;
 	}
 	
-	private function cleanUpData ($input, $flags = ENT_COMPAT | ENT_HTML401, $encoding = 'UTF-8', $double_encode = false) {
-		static $flags, $encoding, $double_encode;
-		if (is_array($input)) {
-			return array_map('htmlspecialchars_recursive', $input);
-		}
-		else if (is_scalar($input)) {
-			return htmlspecialchars($input, $flags, $encoding, $double_encode);
-		}
-		else {
-			return $input;
-		}
-	}
-	
 }
